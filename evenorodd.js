673686c047d6db83a5b43ce524d9406801a2977d
YuvasriS27 <yuvasrisekaran02@gmail.com>
2024-08-19 22:44:22 +0530

    Create evenorodd.js

---
+++ evenorodd.js
@@ -0,0 +1,8 @@
+const num = prompt("Enter a number: ");
+if (num % 2 == 0) 
+{
+  console.log("The given number " + num + " is Even");
+} else
+{
+  console.log("The given number  " + num + " is Odd");
+}
