2c78197a281b35cce9bc9b50f85428202c9d2925
YuvasriS27 <yuvasrisekaran02@gmail.com>
2024-08-19 22:46:20 +0530

    Create positive&negative.js

---
+++ positive&negative.js
@@ -0,0 +1,6 @@
+const num = prompt("Enter a number: ");
+if (num > 0) {
+  console.log("The given number " + num + " is positive");
+} else {
+  console.log("The given number  " + num + " is negative");
+}
